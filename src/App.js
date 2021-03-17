845b492e4a3355f1e52d35a28b41993ee6d741b4
Mikael Niemi <n0nimi00@students.oamk.fi>
2021-03-17 13:21:57 +0200

    Muutoksia

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Shopping list</h1>
+      <form>
+        <label>New item</label>
+        <input type="text"></input>
+        <input type="text"></input>
+        <button>Add</button>
+      </form>
+      <li>
+
+      </li>
     </div>
   );
 }
