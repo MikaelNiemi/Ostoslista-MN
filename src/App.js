e2f890be28260b217ee36542c3f3b6f4bb9513b4
Mikael Niemi <n0nimi00@students.oamk.fi>
2021-03-18 18:27:41 +0200

    delete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ const URL ='http://localhost/ostoslista/'
 function App() {
   const [items,setItems] = useState([]);
   const [item,setItem] = useState('');
-  const [amount,setAmount] = useState('');
 
   useEffect(() => {
     fetch(URL + 'index.php')
@@ -51,6 +50,32 @@ function App() {
     )
   }
 
+  function remove(id) {
+    let status = 0;
+    fetch(URL + 'delete.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: id
+      })
+    })
+    .then(
+      (res) => {
+        if (status === 200) {
+          const newListWithoutRemoved = items.filter((item) => item.id !== id);
+          setItems(newListWithoutRemoved);
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }
+
   return (
     <div className="container">
       <h1>Shopping list</h1>
@@ -58,12 +83,12 @@ function App() {
         <form onSubmit={save}>
           <label>New Item</label>
           <input value={item} onChange={e => setItem(e.target.value)}></input>
-          <input value={amount} onChange={e => setAmount(e.target.value)}></input>
+          <input></input>
           <button>Add</button>
         </form>
       </div>
         {items.map(item => (
-          <li key={item.id}>{item.description}</li>
+          <li key={item.id}>{item.description}<a className="delete" onClick={() => remove(item.id)} href="*">Delete</a></li>
         ))}
     </div>
   );
