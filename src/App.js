b86cacf0c9abb5cd154a78a78241e9de8d61b9a2
Mikael Niemi <n0nimi00@students.oamk.fi>
2021-03-18 18:09:36 +0200

    muutoksia

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const URL ='http://localhost/ostoslista/'
 
 function App() {
   const [items,setItems] = useState([]);
+  const [item,setItem] = useState('');
+  const [amount,setAmount] = useState('');
 
   useEffect(() => {
     fetch(URL + 'index.php')
@@ -18,13 +20,51 @@ function App() {
     )
     }, [])
 
+  function save(e) {
+    e.preventDefault();
+    let status = 0;
+    fetch(URL + 'add.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        description: item
+      })
+    })
+    .then(res => {
+      status = parseInt(res.status);
+      return res.json();
+    })
+    .then(
+      (res) => {
+        if (status === 200) {
+          setItems(items => [...items, res]);
+          setItem('');
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }
+
   return (
     <div className="container">
-      <ol>
+      <h1>Shopping list</h1>
+      <div>
+        <form onSubmit={save}>
+          <label>New Item</label>
+          <input value={item} onChange={e => setItem(e.target.value)}></input>
+          <input value={amount} onChange={e => setAmount(e.target.value)}></input>
+          <button>Add</button>
+        </form>
+      </div>
         {items.map(item => (
           <li key={item.id}>{item.description}</li>
         ))}
-      </ol>
     </div>
   );
 }
